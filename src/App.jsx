85a68cf19b33f24c993f6879582dc471669b4427
vishal mehta <professionalprovishal@gmail.com>
2024-01-09 02:52:58 +0530

    time 2

--- src/App.jsx
+++ src/App.jsx
@@ -3,27 +3,55 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 
-function App() {
-  const [count, setCount] = useState("quote here");
 
+// function App() {
+//   const [count, setCount] = useState("quote here");
+
+//   return (
+//     <>
+//       <h1>Random Quote genrator</h1>
+//       <h1>{count}</h1>
+//       <button
+//         onClick={async () => {
+
+//           let raNum= Math.floor(Math.random()*8000);
+//           const value = await fetch(
+//             "https://jacintodesign.github.io/quotes-api/data/quotes.json"
+//           );
+
+//           const con = await value.json();
+//           console.log(con[raNum].text);
+
+//           setCount(con[raNum].text);
+//         }}
+//       ></button>
+//     </>
+//   );
+// }
+
+function App() {
+  const [first, setfirst] = useState('');
   return (
     <>
-      <h1>Random Quote genrator</h1>
-      <h1>{count}</h1>
+      <h1>hello</h1>
+      <h1>{first}</h1>
       <button
-        onClick={async () => {
+        onClick={() => {
+          fetch(
+             "https://jacintodesign.github.io/quotes-api/data/quotes.json"
+          )
+            .then((res) => {
+              return res.json();
+            })
+            .then((r) => {
+              console.log(r);
 
-          let raNum= Math.floor(Math.random()*8000);
-          const value = await fetch(
-            "https://jacintodesign.github.io/quotes-api/data/quotes.json"
-          );
-
-          const con = await value.json();
-          console.log(con[raNum].text);
-
-          setCount(con[raNum].text);
+              setfirst(r[4].text);
+            });
         }}
-      ></button>
+      >
+        click me
+      </button>
     </>
   );
 }
